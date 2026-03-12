1919d876849c9350d4c3861148ea0207a98b0f82
bassottostefano99-rgb <bassotto.stefano99@gmail.com>
2026-03-12 02:13:11 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs   = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const DIR  = __dirname;
+
+const MIME = {
+  '.html': 'text/html; charset=utf-8',
+  '.js':   'application/javascript',
+  '.json': 'application/json',
+  '.png':  'image/png',
+  '.ico':  'image/x-icon',
+  '.svg':  'image/svg+xml',
+  '.css':  'text/css',
+};
+
+http.createServer((req, res) => {
+  let url = req.url.split('?')[0];
+  if (url === '/') url = '/index.html';
+  const file = path.join(DIR, url);
+  const ext  = path.extname(file);
+
+  fs.readFile(file, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': MIME[ext] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log(`TaskFlow running at http://localhost:${PORT}`));
